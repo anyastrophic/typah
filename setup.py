f9e8986ff3997fdd1f62ce5b3a2aacb0dbb7b687
logix <74055355+logixhot@users.noreply.github.com>
2022-02-16 19:59:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='typah',
-    version='1.0.0',    
+    version='1.0.1',    
     description='An insanely simple application to auto type. Supports multi line, repeat, random key delays, set key delays. Uses OOP',
     url='https://github.com/logixhot/typah',
     author='Logix',
